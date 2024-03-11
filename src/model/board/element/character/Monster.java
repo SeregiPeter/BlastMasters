52f795ea5701e492d86a3a420f91e9a961fbd8e6
G9SIY0 <g9siy0@inf.elte.hu>
2024-03-11 21:02:43 +0100

    Entity osztály alapjának létrehozása.

--- src/model/board/element/character/Monster.java
+++ src/model/board/element/character/Monster.java
@@ -1,7 +1,17 @@
 package model.board.element.character;
 
+import model.board.Board;
 import model.board.element.Entity;
 
-public class Monster extends Entity {
+import java.awt.*;
 
+public abstract class Monster extends Entity {
+    protected Board board;
+
+    public Monster(int x, int y, int width, int height, int velocity, Image image, boolean alive, boolean visible, Board board) {
+        super(x, y, width, height, velocity, image, alive, visible);
+        this.board = board;
+    }
+
+    public abstract void move();
 }
