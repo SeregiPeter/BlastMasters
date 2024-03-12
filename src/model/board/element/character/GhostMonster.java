3192839c44f96d61f2d6ab26eae88d9bd7b814ad
Boldi <ye1brv@inf.elte.hu>
2024-03-12 11:29:11 +0100

    GhostMonster osztály alapjának létrehozása

--- src/model/board/element/character/GhostMonster.java
+++ src/model/board/element/character/GhostMonster.java
@@ -1,5 +1,13 @@
 package model.board.element.character;
+import model.board.Board;
 
+import java.awt.*;
 public class GhostMonster extends Monster {
+    public GhostMonster(int x, int y, int width, int height, int velocity, Image image, boolean alive, boolean visible, Board board) {
+        super(x, y, width, height, velocity, image, alive, visible, board);
+    }
 
+    @Override
+    public void move() {
+    }
 }
