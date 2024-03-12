95d56b8a3792f632686a7e1043381c889c0f24e0
Boldi <ye1brv@inf.elte.hu>
2024-03-12 16:14:30 +0100

    Wall osztály alapjának létrehozása

--- src/model/board/element/field/Wall.java
+++ src/model/board/element/field/Wall.java
@@ -2,6 +2,12 @@ package model.board.element.field;
 
 import model.board.element.Entity;
 
+
+import java.awt.*;
+
 public class Wall extends Entity {
 
+    public Wall(int x, int y, int width, int height, int velocity, Image image, boolean alive, boolean visible) {
+        super(x, y, width, height, velocity, image, alive, visible);
+    }
 }
