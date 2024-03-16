e23aa876883cb981c260aae2ab07652c8311fb2b
Levente <eizeu1@inf.elte.hu>
2024-03-17 00:20:54 +0100

    Bomb osztály nagyrészt implementálva, mergre kész. Size Enum kiegészítést igényel

--- src/model/board/element/deposable/Bomb.java
+++ src/model/board/element/deposable/Bomb.java
@@ -3,8 +3,10 @@ package model.board.element.deposable;
 import model.board.Board;
 import model.board.element.Entity;
 import model.board.element.character.Player;
+import model.board.element.field.Wall;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,6 +17,7 @@ import static model.board.Size.BOMB_WIDTH;
 public class Bomb extends Entity {
     private static int bombCounter;
     private int id;
+    private boolean detonated=false;
     private Player owner;
     private Board board;
     public Bomb(int x, int y, int width, int height, int velocity, Image image, boolean alive, boolean visible, Player owner, Board board) {
@@ -30,20 +33,79 @@ public class Bomb extends Entity {
         Fuse.schedule(new TimerTask() {
             @Override
             public void run() {
-                explode();
+                if(!detonated){
+                    explode();
+                }
+
             }
         }, 4*1000);                     // after 4 sec the bomb explodes
 
 
     }
-    public Entity getAllEntitiesInRange(){
-        return null;
+    public Entity getEntFromXY(int x,int y){
+        ArrayList<Entity> Ents = new ArrayList<>();
+        for (Entity entity : board.getEntities()) {
+            if(entity.getX()==x && entity.getY()==y){
+                Ents.add(entity);
+            }
+        }
+        return Ents.get(Ents.size()-1);
+
     }
     public void explode(){
+        detonated=true;
+        boolean right=contact(getEntFromXY(this.x+TILE_WIDTH.getValue(),this.y));
+        boolean left=contact(getEntFromXY(this.x-TILE_WIDTH.getValue(),this.y));
+        boolean down=contact(getEntFromXY(this.x,this.y+TILE_HEIGHT.getValue()));
+        boolean up=contact(getEntFromXY(this.x,this.y-TILE_HEIGHT.getValue()));
 
-        board.getEntFromXY();
+        Timer Timer=new Timer();
+        Timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(right)contact(getEntFromXY(x+2*TILE_WIDTH.getValue(),y));
+                if(left)contact(getEntFromXY(x-2*TILE_WIDTH.getValue(),y));
+                if(up)contact(getEntFromXY(x,y+2*TILE_HEIGHT.getValue()));
+                if(down)contact(getEntFromXY(x,y-2*TILE_HEIGHT.getValue()));
+            }
+        }, 300);
 
-
+    }
+    private boolean contact(Entity entity) {         //returns true if the explosion can continue in that direction
+        if (entity instanceof Wall) return false;    //and generate fragments on the entity
+        if (entity instanceof Box) {
+            board.getEntities().add(                //generate Bomb fragments (temporary solution)
+                    new Bomb(
+                            entity.getX(),
+                            entity.getY(),
+                            BOMB_WIDTH.getValue(),
+                            BOMB_HEIGHT.getValue(),
+                            0,
+                            null,
+                            false,
+                            true,
+                            null,
+                            this.board));
+            //entity.brake()                        // Box gets destroyed
+            return false;
+        } else if (entity instanceof Bomb) {
+            ((Bomb) entity).explode();
+        } else {
+            board.getEntities().add(                //generate Bomb fragments (temporary solution)
+                    new Bomb(
+                            entity.getX(),
+                            entity.getY(),
+                            BOMB_WIDTH.getValue(),
+                            BOMB_HEIGHT.getValue(),
+                            0,
+                            null,
+                            false,
+                            true,
+                            null,
+                            this.board));
+            return true;
+        }
+        return false;
     }
     public int getId() { return id; }
 
