92e5bdf190025d7797f650f85204c2d487a9497b
Máté <u5bky4@inf.elte.hu>
2024-05-05 23:52:47 +0200

    Tesztelés megalapozása.

---
+++ src/test/java/model/board/DirectionTest.java
@@ -0,0 +1,40 @@
+package model.board;
+
+import junit.framework.TestCase;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+
+public class DirectionTest extends TestCase {
+
+    public void testGetOppositeDirection() {
+        assertEquals(Direction.DOWN, Direction.getOppositeDirection(Direction.UP));
+        assertEquals(Direction.UP, Direction.getOppositeDirection(Direction.DOWN));
+        assertEquals(Direction.RIGHT, Direction.getOppositeDirection(Direction.LEFT));
+        assertEquals(Direction.LEFT, Direction.getOppositeDirection(Direction.RIGHT));
+    }
+
+    public void testGetDirectionExcept() {
+        for (Direction original : Direction.values()) {
+            Set<Direction> possibleResults = EnumSet.allOf(Direction.class);
+            possibleResults.remove(original);
+
+            Set<Direction> receivedResults = EnumSet.noneOf(Direction.class);
+
+            for (int i = 0; i < 100; i++) {
+                Direction result = Direction.getDirectionExcept(original);
+                assertNotNull( "Returned direction should not be null", result);
+                assertNotEquals("Returned direction should not be " + original, original, result);
+                receivedResults.add(result);
+            }
+
+            assertTrue("Should return more than one unique direction for " + original, receivedResults.size() > 1);
+            for (Direction received : receivedResults) {
+                assertTrue( "Received direction should be a valid direction excluding " + original, possibleResults.contains(received));
+            }
+        }
+    }
+}
